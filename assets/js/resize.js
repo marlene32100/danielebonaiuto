50b53989cb517aa3dbe5d5e6645f1724ff7760f5
marlene32100 <67315304+marlene32100@users.noreply.github.com>
2021-11-19 13:39:40 +0100

    Re-fixed resize, adding var(--app-height) in style.css where needed

--- assets/js/resize.js
+++ assets/js/resize.js
@@ -2,6 +2,5 @@ const appHeight = () => {
   const doc = document.documentElement;
   doc.style.setProperty("--app-height", `${window.innerHeight}px`);
 };
-window.addEventListener("load", () => {
-  appHeight();
-});
+window.addEventListener("resize", appHeight);
+appHeight();
