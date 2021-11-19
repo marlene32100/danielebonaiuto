34023fb44e89c82ce291816d7b34539e584972b5
marlene32100 <67315304+marlene32100@users.noreply.github.com>
2021-11-19 13:34:54 +0100

    fixed resize.js

--- assets/js/resize.js
+++ assets/js/resize.js
@@ -2,5 +2,6 @@ const appHeight = () => {
   const doc = document.documentElement;
   doc.style.setProperty("--app-height", `${window.innerHeight}px`);
 };
-window.addEventListener("load", appHeight);
-appHeight();
+window.addEventListener("load", () => {
+  appHeight();
+});
